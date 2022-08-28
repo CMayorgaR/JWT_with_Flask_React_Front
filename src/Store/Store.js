8601d5a4995481423258a30829cd3a0636bd3629
Camilo Mayorga Ríos <camilo@MacBook-Air-de-Camilo.local>
2022-08-27 21:29:19 -0400

    desarrollando componentes

---
+++ src/Store/Store.js
@@ -0,0 +1,13 @@
+const APIusers = "http://localhost:8080/";
+
+const getState = ({ setStore, getActions, getStore }) => {
+  return {
+    store: {
+
+    },
+    actions: {
+    }
+}
+};
+
+export default getState;
