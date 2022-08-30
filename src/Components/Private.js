36908ecfe91dc9db5a88e4ad6979bd5117ad4edc
Camilo Mayorga Ríos <camilo@MacBook-Air-de-Camilo.local>
2022-08-30 16:51:58 -0400

    funcionando signup y login

--- src/Components/Private.js
+++ src/Components/Private.js
@@ -1,4 +1,4 @@
-const Private = () => {
+const Private = () => {    
     return <div>
         <h1>SOY UNA RUTA PRIVADA MÁS TE VALE TENER UN TOKEN DE ACCESO</h1>
     </div>
--- src/Store/Store.js
+++ src/Store/Store.js
@@ -3,72 +3,73 @@ const APIusers = "http://localhost:8080/";
 const getState = ({ setStore, getActions, getStore }) => {
   return {
     store: {
-        user: {
-            email: "",
-            password: "",
-          }
+      user: {
+        email: "",
+        password: "",
+      },
     },
     actions: {
-        handleSignUp: (e) => {
-            const { user } = getStore();
-            setStore({
-              user: {
-                ...user,[e.target.name]: e.target.value},
-            });
+      handleSignUp: (e) => {
+        const { user } = getStore();
+        setStore({
+          user: {
+            ...user, [e.target.name]: e.target.value
           },
-          handleSubmitSignUp: (evento, navigate) => {
-            const actions = getActions();
-            evento.preventDefault();
-            const { user } = getStore();
-            fetch(APIusers + "sign_up", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(user),
-            })
-              .then((res) => res.json())
-              .then((data) => console.log(data));
-            navigate("/log_in")}
+        });
+      },
+      handleSubmitSignUp: (evento, navigate) => {
+        const actions = getActions();
+        evento.preventDefault();
+        const { user } = getStore();
+        fetch(APIusers + "sign_up", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
           },
-          handleChangeLogIn: (evento) => {
-            const { user } = getStore();
-            setStore({
-              user: {
-                ...user,
-                [evento.target.name]: evento.target.value,
-              }
-            })
+          body: JSON.stringify(user),
+        })
+          .then((res) => res.json())
+          .then((data) => console.log(data));
+        navigate("/log_in")
+      },
+      handleChangeLogIn: (evento) => {
+        const { user } = getStore();
+        setStore({
+          user: {
+            ...user,
+            [evento.target.name]: evento.target.value,
+          }
+        })
+      },
+      handleSubmitLogin: (evento, navigate) => {
+        evento.preventDefault();
+        localStorage.clear();
+        const { user } = getStore();
+        fetch(APIusers + "login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
           },
-          handleSubmitLogin: (evento, navigate) => {
-            evento.preventDefault();
-            localStorage.clear();
-            const { user } = getStore(); //traeme el usuario del store
-            fetch(APIusers + "login", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(user),
-            })
-              .then((res) => res.json())
-              .then((data) => {
-                console.log(data);
-                if (data.access_token) {
-                  localStorage.setItem("token", data.access_token);
-                  navigate("/private");
-                } else {
-                  localStorage.clear();
-                }
-              });
-            setStore({
-              user: {
-                email: "",
-                password: "",
-              },
-            });
-          }, 
+          body: JSON.stringify(user),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.access_token) {
+              localStorage.setItem("token", data.access_token);
+              navigate("/private");
+            } else {
+              localStorage.clear()
+            }
+          });
+        setStore({
+          user: {
+            email: "",
+            password: "",
+          },
+        });
+      },
     }
+  }
 };
 
 export default getState;
